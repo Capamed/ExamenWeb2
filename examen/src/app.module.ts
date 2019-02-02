13fbde9fed8e689cbbd3521a4c3c416eeadcec7e
efren sandoval <efren.sandoval@epn.edu.ec>
2019-02-01 20:25:43 -0500

    Cambiando la ubicación a los módulos.
    Colocando los módulos de los directorios en la ubicación correcta.

--- examen/src/app.module.ts
+++ examen/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { RegistroModule } from './registro/registro.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { RolModule } from './rol/rol.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, RegistroModule, UsuarioModule, RolModule],
   controllers: [AppController],
   providers: [AppService],
 })
