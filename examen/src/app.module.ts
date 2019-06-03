1044daeae5e1af1309deea62839c969065461a18
efren sandoval <efren.sandoval@epn.edu.ec>
2019-06-02 21:56:31 -0500

    Hasta aquí llegue

--- examen/src/app.module.ts
+++ examen/src/app.module.ts
@@ -5,9 +5,11 @@ import { LoginModule } from './login/login.module';
 import { RegistroModule } from './registro/registro.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { RolModule } from './rol/rol.module';
+import { AutorModule } from './autor/autor.module';
+import { LibroModule } from './libro/libro.module';
 
 @Module({
-  imports: [LoginModule, RegistroModule, UsuarioModule, RolModule],
+  imports: [LoginModule, RegistroModule, UsuarioModule, RolModule, AutorModule, LibroModule],
   controllers: [AppController],
   providers: [AppService],
 })
